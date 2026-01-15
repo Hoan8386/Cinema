922674bcbcb37e7bb05da62b232dcabf93a41b36
hoan <nguyenthanhhoan300904@gmail.com>
2026-01-15 17:14:44 +0700

    intergrate rate limiting

--- apigateway/src/main/java/com/cinema/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/cinema/apigateway/ApigatewayApplication.java
@@ -2,12 +2,21 @@ package com.cinema.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver userKeyResolver() {
+		// Lấy IP client để làm key limit
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
 	}
 
 }
