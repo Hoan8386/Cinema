a94f9602331192c1942986f3260a10b4421b9544
hoan <nguyenthanhhoan300904@gmail.com>
2026-01-17 22:29:52 +0700

    config kafka and create notification service

---
+++ notificationservice/src/main/java/com/cinema/notificationservice/NotificationserviceApplication.java
@@ -0,0 +1,15 @@
+package com.cinema.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({ "com.cinema.notificationservice", "com.cinema.commonservice" })
+public class NotificationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationserviceApplication.class, args);
+	}
+
+}
